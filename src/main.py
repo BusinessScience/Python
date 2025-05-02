ec291a4351d4d639fd4ddc614bf262ae6f3fb9f8
Business Science <208981291+BusinessScience@users.noreply.github.com>
2025-05-02 12:12:29 +0200

    practice

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ for i in range(10,201):
     else:
         print(i)
 
-word = input("Enter a word: ")
+word = ("Enter a word: ")
 first = 0
 last = len(word)-1
 
@@ -30,3 +30,60 @@ if(is_palindrome):
     print('jest')
 else:
     print('nie')
+
+lista = [1,2,3,4]
+print(lista)
+
+lista_2 = ["cos", True, 4]
+
+print(lista_2)
+print(lista_2[1])
+
+lista_2.append(5)
+print(lista_2)
+print(lista_2[-2])
+print(len(lista_2)/ 2)
+print(len(lista_2)// 2)
+
+lista_3 = []
+lista_3_size = int(input("Wielkośc listy "))
+
+for i in range(0, lista_3_size):
+    elem = int(input('Podaj liczbę'))
+    lista_3.append(elem)
+
+print(lista_3)
+lista_3.reverse()
+print(lista_3)
+print(lista_3[::-1])
+print(lista_3)
+
+n_lista = list(map(lambda x : x *2, lista_3))
+print(n_lista)
+
+lista_4 = [None] * 4
+sum_1 = 0
+for i in range(len(lista_4)):
+    liczba = int(input("Podaj"))
+    sum_1 += liczba
+
+print(sum_1)
+
+nn_list = []
+for i, v in enumerate(lista_3):
+    nn_list.append(v)
+
+print(nn_list)
+
+m_list = [f'indeks: {i}, element: {element}' for i, element in enumerate(lista_3)]
+print(m_list)
+
+import random
+lotto = []
+while len(lotto) < 6:
+    numeber = random.randint(1, 49)
+    if numeber not in lotto:
+        lotto.append(numeber)
+
+lotto.sort()
+print(lotto)
