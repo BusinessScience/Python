1c42397944cdbb8985723580ddce1763b90ccf67
Business Science <208981291+BusinessScience@users.noreply.github.com>
2025-05-02 16:37:41 +0200

    Task_2/ matplotlib

---
+++ src/Task_2_matplotlib.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import pylab as p
+
+x = [1,2,3]
+y = [4,5,6]
+
+plt.plot(x,y)
+plt.grid(True)
+plt.show()
+
+x_1 = range(1,20)
+y_1 = []
+
+for i in x_1:
+    y_1.append(i*3)
+
+plt.plot(x_1,y_1, color='red')
+plt.title('wykres')
+plt.grid(True)
+
+plt.show()
